8bc05a3b28becc66171f43db2170b14fcfa51683
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2020-03-06 14:16:43 -0300

    feat(application): Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+  extends: ['@nuxtjs', 'prettier', 'prettier/vue', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'standard/no-callback-literal': 0,
+  },
+}
---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="https://res.cloudinary.com/nogsantos/image/upload/v1583341771/recruiit/large_recruiit.png" alt="Recruiit" />
+</template>
