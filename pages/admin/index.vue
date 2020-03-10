35471721e66d2051a42c85a9a1c570f7a7d450f6
Guilherme Breda <guilherme.breda@instruct.com.br>
2020-03-10 18:07:05 -0300

    feat(recruiit-app): Adiciona download dos currículos no admin

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div style="padding: 40px;">
     <a-row :gutter="64">
-      <h1 class="text--highlight" style="margin-left: 32px;">Administrador</h1>
+      <h1 class="text--highlight" style="margin-left: 32px; margin-bottom: 0;">Administrador</h1>
+      <h2 style="color: rgba(0, 0, 0, 0.65); margin: 10px 0 40px 32px;">Primeira Etapa: Análise dos currículos</h2>
       <a-col :span="6">
         <h2 class="text--highlight">
           <a-icon type="clock-circle" :style="{ fontSize: '20px', color: 'rgb(0, 153, 255)' }" /> Processando
         </h2>
         <a-card
-          v-for="item in processing"
-          :key="item"
+          v-for="(item, index) in processing"
+          :key="item + index"
           class="card"
           :headStyle="{ 'border-bottom': '1px solid rgb(0, 153, 255)', color: 'rgba(0, 0, 0, 0.65)' }"
           style="margin-top: 32px; border-color: rgb(0, 153, 255);"
@@ -26,8 +27,8 @@
           <a-icon type="check-circle" :style="{ fontSize: '20px', color: 'rgb(51, 204, 51)' }" /> Aprovado
         </h2>
         <a-card
-          v-for="item in approved"
-          :key="item"
+          v-for="(item, index) in approved"
+          :key="item + index"
           class="card"
           :headStyle="{ 'border-bottom': '1px solid rgb(51, 204, 51)', color: 'rgba(0, 0, 0, 0.65)' }"
           style="margin-top: 32px; border-color: rgb(51, 204, 51;"
@@ -42,7 +43,17 @@
             </p>
             <a-tooltip>
               <template slot="title">Download do Currículo</template>
-              <a-button><a-icon type="file-pdf" />Download</a-button>
+              <a-button class="button"
+                ><a-icon type="file-pdf" />
+                <a
+                  class="button__link"
+                  :href="
+                    'https://res.cloudinary.com/nogsantos/image/upload/v1583872871/instruct/' + item.resume + '.pdf'
+                  "
+                  target="_blank"
+                  >Download</a
+                ></a-button
+              >
             </a-tooltip>
           </div>
         </a-card>
@@ -52,8 +63,8 @@
           <a-icon type="close-circle" :style="{ fontSize: '20px', color: 'rgb(255, 51, 0)' }" /> Reprovado
         </h2>
         <a-card
-          v-for="item in reproved"
-          :key="item"
+          v-for="(item, index) in reproved"
+          :key="item + index"
           class="card"
           :headStyle="{ 'border-bottom': '1px solid rgb(255, 51, 0)', color: 'rgba(0, 0, 0, 0.65)' }"
           style="margin-top: 32px; border-color: rgb(255, 51, 0);"
@@ -68,7 +79,17 @@
             </p>
             <a-tooltip>
               <template slot="title">Download do Currículo</template>
-              <a-button><a-icon type="file-pdf" />Download</a-button>
+              <a-button class="button"
+                ><a-icon type="file-pdf" />
+                <a
+                  class="button__link"
+                  :href="
+                    'https://res.cloudinary.com/nogsantos/image/upload/v1583872871/instruct/' + item.resume + '.pdf'
+                  "
+                  target="_blank"
+                  >Download</a
+                ></a-button
+              >
             </a-tooltip>
           </div>
         </a-card>
@@ -78,8 +99,8 @@
           <a-icon type="warning" :style="{ fontSize: '20px', color: 'rgb(255, 153, 0)' }" /> Quarentena
         </h2>
         <a-card
-          v-for="item in quarantine"
-          :key="item"
+          v-for="(item, index) in quarantine"
+          :key="item + index"
           class="card"
           :headStyle="{ 'border-bottom': '1px solid rgb(255, 153, 0)', color: 'rgba(0, 0, 0, 0.65)' }"
           style="margin-top: 32px; border-color: rgb(255, 153, 0);"
@@ -94,7 +115,17 @@
             </p>
             <a-tooltip>
               <template slot="title">Download do Currículo</template>
-              <a-button><a-icon type="file-pdf" />Download</a-button>
+              <a-button class="button"
+                ><a-icon type="file-pdf" />
+                <a
+                  class="button__link"
+                  :href="
+                    'https://res.cloudinary.com/nogsantos/image/upload/v1583872871/instruct/' + item.resume + '.pdf'
+                  "
+                  target="_blank"
+                  >Download</a
+                ></a-button
+              >
             </a-tooltip>
           </div>
         </a-card>
